0e39233eb427f4da28c2e3a19315dba1dfb5c7f8
Max Dvorak <dvorakmax2002@gmail.com>
2024-04-08 13:51:40 +0300

    last changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Remove imports for lazy-loaded components
-
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  // Update the routes to lazy load the modules
   { path: 'home', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
   { path: 'edit', loadChildren: () => import('./pages/edit-page/edit-page.module').then(m => m.EditPageModule) },
   { path: 'about', loadChildren: () => import('./pages/about-me-page/about-me-page.module').then(m => m.AboutMePageModule) },
-  // Add other routes if needed
 ];
 
 @NgModule({
